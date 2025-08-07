eee3c9db1feee7909df2917928b97dca33cf061e
JuliyaPershina <jpershina79@gmail.com>
2025-08-07 12:26:17 +0300

    update4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/exp-rating/', // обов’язково: назва репозиторію
+  base: '/exp-raiting/', // обов’язково: назва репозиторію
   server: {
     host: true,
   },
